c7b7cc452859a11bc1f6f3e9f39ab212e9bb4857
muhazawan04 <86005705+muhazawan04@users.noreply.github.com>
2021-06-28 19:37:42 +0500

    Update task 4.h

--- OOPS/lab 3/task 4/task 4.h
+++ OOPS/lab 3/task 4/task 4.h
@@ -1,1 +1,24 @@
+#ifndef TASK_4_H_INCLUDED
+#define TASK_4_H_INCLUDED
+class myComplex{
+private:
+    double real;
+    double imaginary;
+public:
+    myComplex(); //Default Constructor
+    myComplex(double r, double i);//overloaded constructor
+    myComplex(myComplex &mc); //Copy constructor
+    ~myComplex();
+  
+    void setreal(double r);
+    void setimaginary(double i);
 
+    double getreal();
+    double getimaginary();
+
+    void print();
+
+};
+
+
+#endif // TASK_4_H_INCLUDED
