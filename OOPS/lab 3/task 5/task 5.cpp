2f04a0b21e1ccd9c9ee6ebf5a959f20473181341
muhazawan04 <86005705+muhazawan04@users.noreply.github.com>
2021-06-28 20:41:57 +0500

    Update task 5.cpp

--- OOPS/lab 3/task 5/task 5.cpp
+++ OOPS/lab 3/task 5/task 5.cpp
@@ -1,1 +1,39 @@
+#include "task 5.h"
+#include <iostream>
 
+
+using namespace std;
+    void fraction::setneumerator(int n){
+        neumerator = n;
+    }
+    void fraction::setdenominator(int d){
+        denominator = d;
+    }
+
+    int fraction::getneumerator(){
+        return neumerator;
+    }
+    int fraction::getdenominator(){
+        return denominator;
+    }
+    void fraction::print(){
+        cout << neumerator <<" / "<<denominator;
+    }
+  fraction(){
+    neumerator =0;
+    denominator =0;
+    cout<< "Default constructor invoked"<<endl;
+  }
+  fraction(int n, int d){
+    setneumerator(n);
+    setdenominator(d);
+    cout<< "overloaded constructor invoked"<<endl;
+  }
+  ~fraction(){
+    cout<< "Destructor invoked"<<endl;
+  }
+  fraction(fraction &fr){
+    neumerator = fr.neumerator;
+    denominator = fr.denominator;
+    cout << "Copy Constructor invoked"<<endl;
+  }
