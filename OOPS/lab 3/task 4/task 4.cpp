befbbb93b8cbc76cecf3533ad832b0f32085322f
muhazawan04 <86005705+muhazawan04@users.noreply.github.com>
2021-06-28 19:37:31 +0500

    Update task 4.cpp

--- OOPS/lab 3/task 4/task 4.cpp
+++ OOPS/lab 3/task 4/task 4.cpp
@@ -1,1 +1,48 @@
 
+#include "task 4.h"
+#include <iostream>
+
+using namespace std;
+    myComplex::myComplex(){ //Default construtor
+    real = 0;
+    imaginary =0;
+    }
+    myComplex::myComplex(double r, double i){ //Overloaded Constructor
+        setreal(r);
+        setimaginary(i);
+    }
+   myComplex::myComplex(myComplex &mc){ //Copy constructor
+     real = mc.real;
+     imaginary = mc.iamginary;
+     cout<<"Copy Constructor has been invoked"<<endl;
+   }
+
+    myComplex::~myComplex(){
+      cout << "Destructor has been invoked"<<endl;
+    }
+  
+    void myComplex:: setreal(double r){
+        real=r;
+    }
+    void myComplex::setimaginary(double i){
+        imaginary = i;
+    }
+
+    double myComplex::getreal(){
+        return real;
+    }
+    double myComplex::getimaginary() {
+        return imaginary;
+    }
+
+    
+    void myComplex::print(){
+    if(imaginary < 0)
+        {
+            cout << real << "  "<< imaginary<<"j\n";
+        }
+    else
+        {
+            cout << real << " + "<< imaginary<<"j\n";
+        }
+    }
