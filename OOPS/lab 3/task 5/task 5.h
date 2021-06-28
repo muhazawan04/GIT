f73109c1e9ebdba8e25457f67d56cf088dfdefab
muhazawan04 <86005705+muhazawan04@users.noreply.github.com>
2021-06-28 20:42:03 +0500

    Update task 5.h

--- OOPS/lab 3/task 5/task 5.h
+++ OOPS/lab 3/task 5/task 5.h
@@ -1,1 +1,26 @@
+#ifndef TASK_5_H_INCLUDED
+#define TASK_5_H_INCLUDED
+class fraction{
+///private
+    int neumerator;
+    char sign;
+    int denominator;
+public:
+    fraction();
+    fraction(int n, int d);
+    ~fraction();
+    fraction(fraction &fr);
+  
+    void setneumerator(int n);
+    void setsign(char s);
+    void setdenominator(int d);
 
+    int getneumerator();
+    char getsign();
+    int getdenominator();
+    void print();
+    
+};
+
+
+#endif // TASK_5_H_INCLUDED
